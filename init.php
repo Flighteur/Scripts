f35c679ac387c422c37fd1dc8fef71424a7fe5df
Flighteur <pit.brun@hotmail.fr>
2014-10-03 20:29:38 +0200

    Syncing
    Syncing repo

--- init.php
+++ init.php
@@ -1,16 +1,23 @@
 <?php
-	function init() {
-		//	Initialising Security systems and then databases
-		try {
-			$Users_databases = new PDO('mysql:host=localhost;dbname=Users', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$Sections_database = new PDO('mysql:host=localhost;dbname=Sections', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$Topics_database = new PDO('mysql:host=localhost;dbname=Topics', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$RSS_Topic_flux_generator = new PDO('mysql:host=localhost;dbname=Topic_RSS', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-		}
-		catch (Exception $e)
-		{
-			die('Erreur : '.$e->getMessage());
-		}
+	//	Initialising Security systems and then databases
+	define('DB_HOST', 'localhost');
+	define('DB_ROOT_USER', 'root');
+	define('DB_ROOT_PASS', '');
+	
+	$Users = 'Users';
+	$Sections = 'Sections';
+	$Topics = 'Topics';
+
+	try {
+		
+		/*$Users = new PDO('mysql:host=localhost;dbname=Users', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$Sections = new PDO('mysql:host=localhost;dbname=Sections', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$Topics = new PDO('mysql:host=localhost;dbname=Topics', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$Topics_RSS = new PDO('mysql:host=localhost;dbname=Topic_RSS', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));*/
 	}
-init()
+	catch (Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+}
 ?>
